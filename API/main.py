8fc390b23ec121171886ac506335ce5db511b008
Harsh Raj <harshraj8896@gmail.com>
2022-10-01 05:42:44 +0530

    with a model and fastAPI backend server integrated

---
+++ API/main.py
@@ -0,0 +1,51 @@
+from fastapi import FastAPI, UploadFile, File
+import uvicorn
+from io import BytesIO
+from PIL import Image
+import numpy as np
+import tensorflow as tf
+import os
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+app = FastAPI()
+
+MODEL = tf.keras.models.load_model('../Models/1')
+
+
+prod_model = tf.keras.models.load_model('../Models/1')
+beta_model = tf.keras.models.load_model('../Models/2')
+
+
+
+CLASS_NAMES = ["Early Blight", "Late Blight", "Healthy"]
+
+
+@app.get("/ping")
+async def ping():
+    return "Hello"
+
+
+def read_file_as_image(data) -> np.ndarray:
+    image = np.array(Image.open(BytesIO(data)))
+    return image
+
+
+@app.post("/predict")
+async def predict(
+        file: UploadFile = File(...)
+):
+    image = read_file_as_image(await file.read())
+    img_batch = np.expand_dims(image, 0)
+    predictions = MODEL.predict(img_batch)
+    predicted_class = CLASS_NAMES[np.argmax(predictions[0])]
+    confidence = np.max(predictions[0])
+    print(predicted_class, confidence)
+    return {
+        "class": predicted_class,
+        "confidence": float(confidence)
+    }
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host='localhost', port=8000)
